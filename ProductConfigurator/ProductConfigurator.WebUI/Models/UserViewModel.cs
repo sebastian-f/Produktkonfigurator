ce2d79ea0049d2707abb63e1431b71dcc2588d57
antonbex <antonbex@hotmail.com>
2013-09-13 17:01:42 +0200

    Added CreateAccount För att det ska funka att skapa konto behöver du köra filen aspnet_regsql först

--- ProductConfigurator/ProductConfigurator.WebUI/Models/UserViewModel.cs
+++ ProductConfigurator/ProductConfigurator.WebUI/Models/UserViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ProductConfigurator.WebUI.Models
 {
 	public class UserViewModel
 	{
+		[HiddenInput(DisplayValue=false)]
 		public int Id { get; set; }
+		[Required]
 		public string Username { get; set; }
+		[Required]
 		public string Telephone { get; set; }
+		[Required]
 		public string Email { get; set; }
+		[Required]
 		public string Company { get; set; }
+		[Required]
 		public string StreetName { get; set; }
+		[Required]
 		public string ZipCode { get; set; }
+		[Required]
 		public string City { get; set; }
+		[Required]
 		public string Country { get; set; }
 	}
 }
