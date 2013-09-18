028b925e80f3462ba363cba07e1f0ef56d50472d
Sebastian Forss <sebastianforss@hotmail.com>
2013-09-18 13:42:40 +0200

    Added mailService

---
+++ ProductConfigurator/ProductConfigurator.Services/Interface/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductConfigurator.Services.Interface
+{
+    public interface IMailService
+    {
+        void SendMail(string toEmailAddress, string toName, string subject, string body);
+    }
+}
